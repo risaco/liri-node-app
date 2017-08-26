22aaa1e5134455e9b512dee419e0b284ae820302
risaco <rhia.cooper@gmail.com>
2017-08-25 22:04:42 -0500

    updated keys.js with spotify and omdb keys, updated liri.js

--- keys.js
+++ keys.js
@@ -6,3 +6,13 @@ exports.twitterKeys = {
   access_token_key: '901245364279418880-y6fK1Wd2HjL4beCnK0k078WmCLdfKHf',
   access_token_secret: 'J93dQYkz1AuTCa6Ii2Tk9TJUNqJ5POSCnvzmIV9xQrNjh',
 }
+
+exports.spotifyKeys = {
+  client_ID: 'fc59b166c76a4759a26a13ce7483b2ff',
+  client_secret: '98e748b8cb744facbc4c1ef2fb00f115',
+}
+
+exports.omdbKey = {
+
+	api_key: '40e9cece',
+}
--- liri.js
+++ liri.js
@@ -2,14 +2,18 @@
 var accessKeys = require("./keys.js");
 
 // stores the needed information
-var keys = accessKeys.twitterKeys;
+var twitterKeys = accessKeys.twitterKeys;
+var spotifyKeys = accessKeys.spotifyKeys;
+var omdbKey = accessKeys.ombdKey;
 
 // ******* GET PACKAGES *******
 var Twitter = require('twitter'); // imports the twitter package
 
 var Spotify = require('node-spotify-api'); // imports the spotify package
 
+var request = require('request'); // this file is used to make request to OMDB api
 
+var fs = require('fs');
 
 // assigns variables to user inputs
 var action = process.argv[2];
@@ -18,18 +22,37 @@ var something = process.argv[3];
 
 // action commands
 switch (action) {
+
+	// display last 20 tweets and when they were created
 	case 'my-tweets':
 	break;
 
+	// takes the song provided and displays artist(s), song name, preview link of song, album of song
+	// if no song is provided, use "the sign" by Ace of Bass
 	case 'spotify-this-song':
 	break;
 
+	// takes movie title and displays movie title, year it came out, imdb rating, rotten tomatoes rating,
+	// country where produced, language of the movie, plot, actors
+	// if no movie is entered, default to "Mr. Nobody"
 	case 'movie-this':
 	break;
 
+	// uses fs command to read 'random.txt' and execute one of LIRI's commands
 	case 'do-what-it-says':
+	fs.readFile("random.txt", "utf8", function(error, data) {
+
+		// create array to split text string and hold values
+		var dataArray = data.split(',');
+
+		// assign data in array to appropriate variables
+		action = dataArray[0];
+		something = dataArray[1];
+	}
 	break;
 
 	case default:
 	console.log("Invalid command. Try again.");
 }
+
+// ******* FUNCTIONS *******
